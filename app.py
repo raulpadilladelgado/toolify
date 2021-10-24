3aaedde8a225d9f675b6d1b02238ba0450ab9cb0
Raúl Padilla <rpd27@outlook.com>
2021-10-24 23:12:50 +0100

    make redirect_uri dynamic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 
+import flask
 import spotipy
 from flask import Flask, render_template, request, url_for
 from spotipy.oauth2 import SpotifyOAuth
@@ -13,16 +14,15 @@ scopes = ["playlist-modify-private",
           "playlist-modify-public",
           "playlist-read-collaborative"]
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
-                                               client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
-                                               redirect_uri=os.environ.get('SPOTIFY_REDIRECT_URI'),
-                                               scope=scopes))
-
-playlist_operator = PlaylistOperator(sp)
-
 
 @app.route("/")
 def list_playlists():
+    host_url = flask.request.host_url
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
+                                                   client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
+                                                   redirect_uri=host_url + 'callback',
+                                                   scope=scopes))
+    playlist_operator = PlaylistOperator(sp)
     user_playlists = playlist_operator.list_user_playlists()
     return render_template(
         "index.html",
@@ -33,5 +33,17 @@ def list_playlists():
 
 @app.route('/order', methods=['POST'])
 def order_playlists():
+    host_url = flask.request.host_url
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
+                                                   client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
+                                                   redirect_uri=host_url + 'callback',
+                                                   scope=scopes))
+    playlist_operator = PlaylistOperator(sp)
     playlist_operator.reorder_playlist_by_release_date(request.form['playlist'])
     return list_playlists()
+
+
+@app.route('/aguacate')
+def aguacate():
+    print(flask.request.host_url)
+    return render_template('index.html')
