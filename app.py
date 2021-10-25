67ea04254d1d5da23a0c08b53f1efcace7bd8529
Raúl Padilla <rpd27@outlook.com>
2021-10-25 16:23:34 +0100

    login with session cookie and refresh access

--- app.py
+++ app.py
@@ -1,49 +1,80 @@
 import os
-
-import flask
+import time
 import spotipy
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect, session
 from spotipy.oauth2 import SpotifyOAuth
-
 from application.PlaylistOperator import PlaylistOperator
 
 app = Flask(__name__)
-
 scopes = ["playlist-modify-private",
           "playlist-read-private",
           "playlist-modify-public",
           "playlist-read-collaborative"]
+TOKEN_INFO = "token_info"
+app.secret_key = os.environ.get('TOOLIFY_SECRET_KEY')
 
 
 @app.route("/")
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+
+@app.route('/redirect')
+def redirect_page():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('list_playlists', _external=True))
+
+
+@app.route("/list")
 def list_playlists():
-    host_url = flask.request.host_url
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
-                                                   client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
-                                                   redirect_uri=os.environ.get('SPOTIFY_REDIRECT_URI'),
-                                                   scope=scopes))
+    try:
+        token_info = get_token()
+    except:
+        print('user not logged in')
+        redirect('/')
+    sp = spotipy.Spotify(auth=token_info['access_token'])
     playlist_operator = PlaylistOperator(sp)
     user_playlists = playlist_operator.list_user_playlists()
     return render_template(
         "index.html",
         playlists=user_playlists,
-        order_playlists_url=url_for('order_playlists')
+        order_playlists_url=url_for('order_playlists', _external=True)
     )
 
 
 @app.route('/order', methods=['POST'])
 def order_playlists():
-    host_url = flask.request.host_url
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
-                                                   client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
-                                                   redirect_uri=host_url + 'callback',
-                                                   scope=scopes))
+    try:
+        token_info = get_token()
+    except:
+        print('user not logged in')
+        redirect('/')
+    sp = spotipy.Spotify(auth=token_info['access_token'])
     playlist_operator = PlaylistOperator(sp)
     playlist_operator.reorder_playlist_by_release_date(request.form['playlist'])
     return list_playlists()
 
 
-@app.route('/aguacate')
-def aguacate():
-    print(flask.request.host_url)
-    return render_template('index.html')
+def create_spotify_oauth():
+    return SpotifyOAuth(client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
+                        client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
+                        redirect_uri=url_for('redirect_page', _external=True),
+                        scope=scopes)
+
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now
+    if is_expired:
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        return token_info
