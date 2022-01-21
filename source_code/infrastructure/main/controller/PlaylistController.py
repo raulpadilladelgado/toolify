9cd1f4867b72c77ce03dd61cf27f37a486d698ed
Raúl Padilla Delgado <rpd27@outlook.com>
2022-01-21 17:24:09 +0000

    extract login functionalities to an controller

--- source_code/infrastructure/main/controller/PlaylistController.py
+++ source_code/infrastructure/main/controller/PlaylistController.py
@@ -1,42 +1,18 @@
-import os
-import time
-
 import spotipy
-from flask import render_template, request, url_for, redirect, session
-from spotipy.oauth2 import SpotifyOAuth
+from flask import render_template, request, url_for
 
 from source_code.application.main.use_cases.ListUserPlaylists import ListUserPlaylists
 from source_code.application.main.use_cases.ReorderPlaylistByReleaseDate import ReorderPlaylist
 from source_code.domain.main.services.ReorderByReleaseDate import ReorderByReleaseDate
-from source_code.infrastructure.main.exceptions.LoginError import LoginError
+from source_code.infrastructure.main.controller.LoginController import LoginController
 
-scopes = ["playlist-modify-private",
-          "playlist-read-private"]
-TOKEN_INFO = "token_info"
-
-
-def login():
-    sp_oauth = create_spotify_oauth()
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
-
-def redirect_page():
-    sp_oauth = create_spotify_oauth()
-    session.clear()
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    session[TOKEN_INFO] = token_info
-    return redirect(url_for('list_playlists', _external=True))
+login_controller = LoginController()
 
 
 def list_playlists():
-    try:
-        token_info = get_token()
-    except LoginError:
-        return redirect(url_for('login', _external=True))
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    list_user_playlists = ListUserPlaylists(sp)
+    token_info = login_controller.get_token_info()
+    spotify_client = spotipy.Spotify(auth=token_info['access_token'])
+    list_user_playlists = ListUserPlaylists(spotify_client)
     user_playlists = list_user_playlists.apply()
     return render_template(
         "index.html",
@@ -46,31 +22,9 @@ def list_playlists():
 
 
 def order_playlists():
-    try:
-        token_info = get_token()
-    except LoginError:
-        return redirect(url_for('login', _external=True))
+    token_info = login_controller.get_token_info()
     sp = spotipy.Spotify(auth=token_info['access_token'])
     reorderer = ReorderByReleaseDate()
     reorder_playlist = ReorderPlaylist(sp, request.form['playlist'], reorderer)
     reorder_playlist.apply()
     return list_playlists()
-
-
-def create_spotify_oauth():
-    return SpotifyOAuth(client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
-                        client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
-                        redirect_uri=url_for('redirect_page', _external=True),
-                        scope=scopes)
-
-
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
-    if not token_info:
-        raise LoginError("Not token info is found")
-    now = int(time.time())
-    is_expired = token_info['expires_at'] - now
-    if is_expired:
-        sp_oauth = create_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-        return token_info
