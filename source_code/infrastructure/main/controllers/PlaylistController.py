71f538c814d27012dbdfa4153071cc9ec9f5d20c
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-09-11 00:59:35 +0100

    remake the user login

--- source_code/infrastructure/main/controllers/PlaylistController.py
+++ source_code/infrastructure/main/controllers/PlaylistController.py
@@ -1,45 +1,52 @@
-import spotipy
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, redirect
 
 from source_code.application.main.usecases.ListUserPlaylists import ListUserPlaylists
 from source_code.application.main.usecases.RemoveDuplicatedSongs import RemoveDuplicatedSongs
 from source_code.application.main.usecases.RemoveNonRemixSongs import RemoveNonRemixSongs
 from source_code.application.main.usecases.ReorderPlaylistByReleaseDate import ReorderPlaylistByReleaseDate
-from source_code.domain.main.valueobjects.Playlists import Playlists
 from source_code.infrastructure.main.adapters.SpotifyWrapperWithSpotipy import SpotifyWrapperWithSpotipy
-from source_code.infrastructure.main.controllers.LoginController import LoginController
+from source_code.infrastructure.main.controllers.LoginController import get_client
 
 
-def list_playlists() -> str:
+def list_playlists():
+    client = get_client()
+    user_is_not_logged = client is None
+    if user_is_not_logged:
+        return redirect(url_for('login'))
+    playlists = ListUserPlaylists(SpotifyWrapperWithSpotipy(client)).apply()
     return render_template(
         "index.html",
-        playlists=(list_user_playlists().values()),
-        list_playlists_url=url_for('list_playlists', _external=True),
-        order_playlists_url=url_for('order_playlists', _external=True),
-        remove_duplicated_songs_url=url_for('remove_duplicated_songs', _external=True),
-        remove_non_remix_songs_url=url_for('remove_non_remix_songs', _external=True)
+        playlists=playlists.values(),
+        list_playlists_url=url_for('list_playlists'),
+        order_playlists_url=url_for('order_playlists'),
+        remove_duplicated_songs_url=url_for('remove_duplicated_songs'),
+        remove_non_remix_songs_url=url_for('remove_non_remix_songs'),
+        sign_out_url=url_for('sign_out')
     )
 
 
-def list_user_playlists() -> Playlists:
-    return ListUserPlaylists(SpotifyWrapperWithSpotipy(get_spotify_client())).apply()
-
-
 def order_playlists():
-    ReorderPlaylistByReleaseDate(SpotifyWrapperWithSpotipy(get_spotify_client()), request.form['playlist']).apply()
+    client = get_client()
+    user_is_not_logged = client is None
+    if user_is_not_logged:
+        return redirect(url_for('login'))
+    ReorderPlaylistByReleaseDate(SpotifyWrapperWithSpotipy(client), request.form['playlist']).apply()
     return "OK"
 
 
 def remove_duplicated_songs():
-    RemoveDuplicatedSongs(SpotifyWrapperWithSpotipy(get_spotify_client())).apply(request.form['playlist'])
+    client = get_client()
+    user_is_not_logged = client is None
+    if user_is_not_logged:
+        return redirect(url_for('login'))
+    RemoveDuplicatedSongs(SpotifyWrapperWithSpotipy(client)).apply(request.form['playlist'])
     return "OK"
 
 
 def remove_non_remix_songs():
-    RemoveNonRemixSongs(SpotifyWrapperWithSpotipy(get_spotify_client())).apply(request.form['playlist'])
+    client = get_client()
+    user_is_not_logged = client is None
+    if user_is_not_logged:
+        return redirect(url_for('login'))
+    RemoveNonRemixSongs(SpotifyWrapperWithSpotipy(client)).apply(request.form['playlist'])
     return "OK"
-
-
-def get_spotify_client():
-    access_token = LoginController.get_token_info()['access_token']
-    return spotipy.Spotify(auth=access_token)
