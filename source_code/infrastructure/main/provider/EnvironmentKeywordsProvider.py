ccc778e7471c0afe8725cec825c9bd458d7eff8a
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2025-02-18 10:25:04 +0000

    Receive remix keywords from env

--- source_code/infrastructure/main/provider/EnvironmentKeywordsProvider.py
+++ source_code/infrastructure/main/provider/EnvironmentKeywordsProvider.py
@@ -1,6 +1,11 @@
 from source_code.domain.main.provider.KeywordsProvider import KeywordsProvider
-
+from dotenv import load_dotenv
+import os
 
 class EnvironmentKeywordsProvider(KeywordsProvider):
+    def __init__(self) -> None:
+        load_dotenv()
+
     def get_keywords(self) -> list[str]:
-        return ["remix"]
+        keywords = os.getenv("KEYWORDS", "")
+        return keywords.split(",") if keywords else []
